e12df389b2d0ce8f69d3a46787454f7ef7583430
Ethan Abercrombie <abercrombieethan@gmail.com>
2023-06-30 15:31:54 -0500

    Code modified to work with rgbif multi-species occurrence extraction.

--- Code/Occurence Data Cleaning/Occurrence_cleaning_function.R
+++ Code/Occurence Data Cleaning/Occurrence_cleaning_function.R
@@ -14,91 +14,59 @@ states <- st_transform(st_read('/Users/ethanabercrombie/Desktop/Spatial_Data/gad
          NAME_1 != 'Alaska' &
            NAME_1 != 'Hawaii')
 
-#Define species in study.
-
-species_list <- c('Peromyscus_maniculatus_sonoriensis',
-'Sorex_ornatus',
-                                    'Dipodomys_heermanni',
-                                    'Microtus_californicus',
-                                    'Reithrodontomys_megalotis',
-                                    'Chaetodipus_californicus',
-                                    'Neotoma_fuscipes',
-                                    'Neotoma_macrotis',
-                                    'Peromyscus_truei',
-                                    'Sciurus_griseus',
-                                    'Dipodomys_agilis',
-                                    'Tamias_merriami',
-                                    'Peromyscus_boylii',
-                                    'Thomomys_bottae',
-                                    'Otospermophilus_beecheyi',
-                                    'Sorex_trowbridgii',
-                                    'Tamias_quadrimaculatus',
-                                    'Sorex_vagrans',
-                                    'Tamias_senex',
-                                    'Tamiasciurus_douglasii',
-                                    'Zapus_princeps',
-                                    'Microtus_montanus',
-                                    'Microtus_longicaudus',
-                                    'Thomomys_monticola',
-                                    'Neotoma_cinerea',
-                                    'Tamias_speciosus',
-                                    'Tamias_amoenus',
-                                    'Sorex_palustris',
-                                    'Marmota_flaviventris',
-                                    'Urocitellus_beldingi',
-                                    'Callospermophilus_lateralis',
-                                    'Sorex_monticolus',
-                                    'Ochotona_princeps',
-                                    'Tamias_alpinus')
+#Load occurrence data downloaded from GBIF.
+occurrence_data <- read.delim('/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/new_occurrence_data/occurrence_data_raw.csv')
 
 #Create empty dataframe to store filtering results.
 #The values are added in the occurrence_filtering function.
-occurrence_metadata <- tibble(species = species_list,
+occurrence_metadata <- tibble(species = str_replace_all(unique(occurrence_data$species),
+                                                        ' ',
+                                                        "_"),
                               filtered_records = NA,
                               within_range = NA,
                               within_buffer = NA,
                               outside_buffer = NA)
 
-###########
-#Plotting function.
-##########
-
-occurrence_plot <- function(){
-  occurrence_map <- ggplot() +
-    labs(title = paste(species_name)) +
-    geom_sf(data = states,
-            fill = NA) +
-    geom_sf(data = rangeMap,
-            color = 'darkgreen',
-            fill = 'green',
-            alpha = 0.5,
-            inherit.aes = FALSE) +
-    geom_sf(data = range_buffer,
-            color = 'yellow',
-            fill = NA,
-            inherit.aes = F) +
-    theme() +
-    coord_sf(xlim = c(pmin(occ_bounding_box[1],
-                           rangeMap_bounding_box[1]),
-                      pmax(occ_bounding_box[3],
-                           rangeMap_bounding_box[3])),
-             ylim = c(pmin(occ_bounding_box[2],
-                           rangeMap_bounding_box[2]),
-                      pmax(occ_bounding_box[4],
-                           rangeMap_bounding_box[4])),
-             expand = T)
-  
-  
-  quartz(type = 'pdf',
-         dpi = 144,
-         antialias = T,
-         file = paste0(plot_export_folder,
-                       species_name))
-  
-  print(occurrence_map)
-  
-  graphics.off()
-}
+# ###########
+# #Plotting function.
+# ##########
+# 
+# occurrence_plot <- function(){
+#   occurrence_map <- ggplot() +
+#     labs(title = paste(species_name)) +
+#     geom_sf(data = states,
+#             fill = NA) +
+#     geom_sf(data = rangeMap,
+#             color = 'darkgreen',
+#             fill = 'green',
+#             alpha = 0.5,
+#             inherit.aes = FALSE) +
+#     geom_sf(data = range_buffer,
+#             color = 'yellow',
+#             fill = NA,
+#             inherit.aes = F) +
+#     theme() +
+#     coord_sf(xlim = c(pmin(occ_bounding_box[1],
+#                            rangeMap_bounding_box[1]),
+#                       pmax(occ_bounding_box[3],
+#                            rangeMap_bounding_box[3])),
+#              ylim = c(pmin(occ_bounding_box[2],
+#                            rangeMap_bounding_box[2]),
+#                       pmax(occ_bounding_box[4],
+#                            rangeMap_bounding_box[4])),
+#              expand = T)
+#   
+#   
+#   quartz(type = 'pdf',
+#          dpi = 144,
+#          antialias = T,
+#          file = paste0(plot_export_folder,
+#                        species_name))
+#   
+#   print(occurrence_map)
+#   
+#   graphics.off()
+# }
 
 #########
 #Cleaning Function
@@ -107,14 +75,14 @@ occurrence_plot <- function(){
 occurrence_cleaning <- function(){
   species <- occurrence_data %>% 
     mutate(gbifID = as.character(gbifID)) %>% 
-    subset(type=='PhysicalObject' |
-             type=='specimen' |
-             type=='PRESERVED_SPECIMEN' |
-             type=='PreservedSpecimen' |
-             type=='Occurrence' |
-             type=="Collection" |
-             type=='Physical Object' |
-             type == 'Objeto fÃsico') %>% #Select physical specimens and occurrences.
+    subset(basisOfRecord=='PhysicalObject' |
+             basisOfRecord=='specimen' |
+             basisOfRecord=='PRESERVED_SPECIMEN' |
+             basisOfRecord=='PreservedSpecimen' |
+             basisOfRecord=='Occurrence' |
+             basisOfRecord=="Collection" |
+             basisOfRecord=='Physical Object' |
+             basisOfRecord == 'Objeto fÃsico') %>% #Select physical specimens and occurrences.
     mutate_if(is.character, list(~na_if(.,""))) %>% #This removes empty coordinates stored as a blankspace "".
     filter(!is.na(decimalLatitude) & 
              !is.na(decimalLongitude))
@@ -159,14 +127,8 @@ occurrence_cleaning <- function(){
                      issue))
     )
   
-  write_csv(speciesSf_filtered,
-            file = paste0(data_export_folder,
-                          species_name,
-                          '_occurrence_cleaned',
-                          '.csv'))
-  
-  occ_bounding_box <<- as.numeric(st_bbox(speciesSf_filtered))
-  rangeMap_bounding_box <<- as.numeric(st_bbox(range_buffer))
+  # occ_bounding_box <<- as.numeric(st_bbox(speciesSf_filtered))
+  # rangeMap_bounding_box <<- as.numeric(st_bbox(range_buffer))
   
   occurrence_metadata[occurrence_metadata$species == species_name, 'filtered_records'] <<- nrow(speciesSf_filtered)
   occurrence_metadata[occurrence_metadata$species == species_name, 'within_range'] <<- nrow(filter(speciesSf_filtered,
@@ -179,12 +141,14 @@ occurrence_cleaning <- function(){
 }
 
 #Define export folders.
-data_export_folder <- '/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/occurrence_data/occurrence_data_clean/'
-plot_export_folder <- '/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/occurrence_data/occurrence_data_clean_maps/'
+data_export_folder <- '/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/new_occurrence_data/'
+#plot_export_folder <- '/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/occurrence_data/occurrence_data_clean_maps/'
 
-for (i in 1:length(species_list)) {
-  species_name <- species_list[i]
-  occurrence_data <- read.delim(paste0('/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/occurrence_data/',species_name,'/occurrence.txt'))
+for (i in 1:length(unique(occurrence_data$species))) {
+  species_name<- str_replace_all(unique(occurrence_data$species)[i],
+                                  ' ',
+                                  "_")
+  
   rangeMap <- st_transform(sf::st_read(paste0('/Users/ethanabercrombie/Desktop/Grinnell-Mammal-Community-Shifts/Data/IUCN Species Ranges/Species Ranges_Exports_QGIS/',
                                               species_name,
                                               '/',
@@ -194,8 +158,13 @@ for (i in 1:length(species_list)) {
   
   speciesSf_filtered <- occurrence_cleaning()
   
-  occurrence_plot()
+  write_csv(speciesSf_filtered,
+            file = paste0(data_export_folder,
+                          species_name,
+                          '_occurrence_cleaned',
+                          '.csv'))
 }
+
 
 #Save metadata file
 occurrence_metadata <- occurrence_metadata %>% 
